99c23c2a5b6b736de6a60683cb5c2453fc72ec18
tuanmjnh <minhtuan200990@gmail.com>
2019-08-08 20:57:51 +0700

    tm

--- src/config/oracle.js
+++ src/config/oracle.js
@@ -1,13 +1,4 @@
 module.exports = {
-  hrPool: {
-    // poolAlias: 'hrPool',
-    user: process.env.HR_USER,
-    password: process.env.HR_PASSWORD,
-    connectString: process.env.HR_CONNECTIONSTRING,
-    poolMin: 10,
-    poolMax: 10,
-    poolIncrement: 0
-  },
   dulieucbg: {
     // poolAlias: 'dulieubkn',
     user: "DULIEU_CBG",
